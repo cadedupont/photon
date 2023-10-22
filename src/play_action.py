766f099fe7f89829cc846aec599d20eb56517230
Cade DuPont <cadedupont45@gmail.com>
2023-10-22 18:07:11 -0500

    Account for different OS when playing sounds

--- src/play_action.py
+++ src/play_action.py
@@ -5,6 +5,11 @@ import tkinter as tk
 import pygubu
 import cv2
 import playsound
+import os
+
+# Import winsound if on Windows
+if os.name == "nt":
+    import winsound
 
 # Load the UI file and create the builder
 builder: pygubu.Builder = pygubu.Builder()
@@ -34,6 +39,12 @@ def update_video(video_label: tk.Label, cap: cv2.VideoCapture, frame_rate: int, 
         update_video(video_label, cap, frame_rate, video_width, video_height)
 
 def build(root: tk.Tk, users: Dict) -> None:
+    # Based on OS, play the countdown sound
+    if os.name == "nt":
+        winsound.PlaySound("res/countdown.wav", winsound.SND_ASYNC)
+    else:
+        playsound.playsound("res/countdown.wav", False)
+
     # Place the main frame in the center of the root window
     main_frame: tk.Frame = builder.get_object("master", root)
     main_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
@@ -67,9 +78,6 @@ def build(root: tk.Tk, users: Dict) -> None:
 
     # Initialize the countdown time
     seconds: int = 30
-
-    # Play the countdown sound
-    playsound.playsound("res/jeopardy.mp3", block=False)
 
     # Start the countdown
     update_timer(timer_label, seconds, main_frame)
