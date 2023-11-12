a8f8dfa4a71dcb4dce524193ef65833c5d42cc99
Vishal Jeyam <98559241+vjeyam@users.noreply.github.com>
2023-11-12 15:25:53 -0600

    Adding game logic 2 (#18)
    * Implemented player hit logic
    
    * fixed logic for red_base and green_base hit
    
    * adding documentation/comments
    
    * added logic to player and base hit methods
    
    * Tying up a few loose ends.
    
    ---------
    
    Co-authored-by: Thomas Buser <tjbuser@uark.edu>

--- src/game_logic.py
+++ src/game_logic.py
@@ -2,36 +2,81 @@
 # Date Created: 15 September 2023
 # Purpose: Module to handle game state including who hit who and what bases have been scored.
 
-from typing import Dict
+from typing import Dict, List
+from user import User
+
+
+# class User:
+#     equipment_id: int
+#     user_id: int
+#     username: str
+#     game_score: int
+#     has_hit_base: bool
+
+# users: Dict[str, List[User]] = {
+#     "green": [],
+#     "red": []
+# }
+
+POINTS_PER_TAG: int = 10
+POINTS_PER_BASE_HIT: int = 100
 
 class GameState:
-    def __init__(self) -> None:
-        self.users = {}
+    def __init__(self, users_dict: Dict[str, List[User]]) -> None:
+        # User references
+        self.green_users = users_dict["green"]
+        self.red_users = users_dict["red"]
+        # Red equipment IDs
+        self.red_user_equipment_ids = []
+        for user in self.red_users:
+            self.red_user_equipment_ids.append(user.equipment_id)
+       # Green equipment IDs
+        self.green_user_equipment_ids = []
+        for user in self.green_users:
+            self.green_user_equipment_ids.append(user.equipment_id)
+        # Team scores, set to default of zero
         self.red_team_score: int = 0
         self.green_team_score: int = 0
-        self.red_base_score_valid: bool = True
-        self.green_base_score_valid: bool = True
+        # If bases are scored
         self.red_base_scored: bool = False
         self.green_base_scored: bool = False
+        self.game_event_list: [str] = []
 
-    def set_users(self, users: Dict[str, Dict[int, tuple[int, str]]]) -> None:
-        self.users = users
+    def player_hit(self, equipment_shooter_code: int, equipment_hit_code: int) -> None:
+        # Attributing points to a green user
+        for user in self.green_users:
+            # Check if id matches, and if player doesn't hit own teammate
+            if user.equipment_id == equipment_shooter_code and equipment_hit_code not in self.green_user_equipment_ids:
+                user.game_score += POINTS_PER_TAG
+                self.green_team_score += POINTS_PER_TAG
+                for victim_user in self.red_users:
+                    if user.equipment_id == equipment_hit_code:
+                        shot_user: User = victim_user
+                self.game_event_list.append(f"{user.username} hit {shot_user.username}")
+        
+        # Attributing points to a red user
+        for user in self.red_users:
+            # Check if id matches, and if player doesn't hit own teammate
+            if user.equipment_id == equipment_shooter_code and equipment_hit_code not in self.red_user_equipment_ids:
+                user.game_score += POINTS_PER_TAG
+                self.red_team_score += POINTS_PER_TAG
+                for victim_user in self.green_users:
+                    if user.equipment_id == equipment_hit_code:
+                        shot_user: User = victim_user
+                self.game_event_list.append(f"{user.username} hit {shot_user.username}")
 
-    def player_hit(self, equipment_shooter_code: int, equipment_hit_code: int):
-        pass
+    def red_base_hit(self, equipment_shooter_code: int) -> None:
+        for user in self.green_users:
+            # Check if id matches, and then add 100 points to the green player and team
+            if user.equipment_id == equipment_shooter_code:
+                user.game_score += POINTS_PER_BASE_HIT
+                self.green_team_score += POINTS_PER_BASE_HIT
+                self.game_event_list.append(f"{user.username} hit red base")
 
-    def red_base_hit(self, equipment_shooter_code: int):
-        if self.red_base_hit_valid:
-            self.red_base_hit = True
-            # TODO: Attribute points to player and team
-            self.red_base_hit_valid = False
-        else:
-            pass
-
-    def green_base_hit(self, equipment_shooter_code: int):
-        if self.green_base_hit_valid:
-            self.green_base_hit = True
-            # TODO: Attribute points to player and team
-            self.green_base_hit_valid = False
-        else:
-            pass
+    def green_base_hit(self, equipment_shooter_code: int) -> None:
+        for user in self.red_users:
+            # Check if id matches, and then add 100 points to the red player and team
+            if user.equipment_id == equipment_shooter_code:
+                user.game_score += POINTS_PER_BASE_HIT
+                self.red_team_score += POINTS_PER_BASE_HIT
+                self.game_event_list.append(f"{user.username} hit green base")
