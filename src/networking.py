e34f14c3d598043e278f6e379a25dbd7be6a85ec
Vishal Jeyam <vishaljeyam@icloud.com>
2023-11-12 13:13:00 -0600

    fixing error checking on  networking.py methods

--- src/networking.py
+++ src/networking.py
@@ -16,58 +16,48 @@ TRANSMIT_PORT: int = 7501
 RECIEVE_PORT: int = 7500
 
 class Networking:
-    def __init__(self) -> None:
+    def __init__(self) -> bool:
         # Using python BSD socket interface
-        # Error Checking for transmiting socket
+        # Error Checking for transmiting socket and recieving socket
         try:
             self.transmit_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        except Exception as e:
-            print(e)
-        
-        # Error Checking for recieving socket
-        try:
             self.recieve_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            return True
         except Exception as e:
             print(e)
-
-    def transmit_equipment_code(self, equipment_code: str) -> None:
+            return False
+    
+    def transmit_equipment_code(self, equipment_code: str) -> bool:
         # This is using the python BSD interface. The 1 enables broadcast at the syscall level and privledged process.
         # Error Checking for transmitting equipment code
         try:
             self.transmit_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        except Exception as e:
-            print(e)
-        
-        try:
             self.transmit_socket.sendto(str.encode(str(equipment_code)), (BROADCAST_ADDRESS, TRANSMIT_PORT))
+            return True
         except Exception as e:
             print(e)
+            return False
     
-    def transmit_start_game_code(self) -> None:
+    def transmit_start_game_code(self) -> bool:
         # Error Checking for transmitting start game code
         try:
             self.transmit_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        except Exception as e:
-            print(e)
-        
-        try:
             self.transmit_socket.sendto(str.encode(str(START_GAME_CODE)), (BROADCAST_ADDRESS, TRANSMIT_PORT))
+            return True
         except Exception as e:
             print(e)
+            return False
             
-    def transmit_end_game_code(self) -> None:
-        # TODO : Eventually add error checking
+    def transmit_end_game_code(self) -> bool:
         # Error Checking for transmitting end game code
         try:
             self.transmit_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        except Exception as e:
-            print(e)
-        
-        try:
             self.transmit_socket.sendto(str.encode(str(END_GAME_CODE)), (BROADCAST_ADDRESS, TRANSMIT_PORT))
+            return True
         except Exception as e:
             print(e)
-        
+            return False
+
     def transmit_player_hit(self, player_code: int) -> None:
         pass
 
